22f7c2636853c62f3b0b8769571c5b9f5ad4823c
YG0122 <yoonkyong0122@gmail.com>
2021-12-28 16:16:13 +0900

    bug fix: delay 1sec at start

--- sistarterTimer/app/src/main/java/com/example/sistartertimer/MainActivity.kt
+++ sistarterTimer/app/src/main/java/com/example/sistartertimer/MainActivity.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import kotlin.concurrent.timer
+//import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
     private var time = 0
     private var isRunning = false
-    private var timerTask: Timer? = null
+    private var timer: Timer? = null
     private var index: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,68 +39,55 @@ class MainActivity : AppCompatActivity() {
         timeIn.visibility = View.GONE
         timeOut.visibility = View.VISIBLE
 
-        hourOut.text = hourIn.text.toString()
-        minOut.text = minIn.text.toString()
-        secOut.text = secIn.text.toString()
+        var hour = strToInt(hourIn.text.toString())
+        var min = strToInt(minIn.text.toString())
+        var sec = strToInt(secIn.text.toString())
 
-        var hour: Int = if (hourIn.text.toString() == "") {
-            0
-        } else {
-            hourIn.text.toString().toInt()
-        }
-        var min: Int = if (minIn.text.toString() == "") {
-            0
-        } else {
-            minIn.text.toString().toInt()
-        }
-        var sec: Int = if (secIn.text.toString() == "") {
-            0
-        } else {
-            secIn.text.toString().toInt()
-        }
+        hourOut.text = formatTime(hour)
+        minOut.text = formatTime(min)
+        secOut.text = formatTime(sec)
 
+        val timer = Timer()
+        timer.schedule(object : TimerTask(){
 
-        timerTask = timer(period = 1000) {
+            override fun run() {
+                time++
 
-            //if (sec == 0 && min == 0 && hour == 0) timerTask?.cancel()
-
-            time++
-
-            when {
-                sec != 0 -> sec--
-                min != 0 -> {
-                    sec = 60
-                    sec--
-                    min--
+                when {
+                    sec != 0 -> sec--
+                    min != 0 -> {
+                        sec = 60
+                        sec--
+                        min--
+                    }
+                    hour != 0 -> {
+                        min = 60
+                        hour--
+                        min--
+                        sec = 60
+                        sec--
+                    }
                 }
-                hour != 0 -> {
-                    min = 60
-                    hour--
-                    min--
-                    sec = 60
-                    sec--
+
+                runOnUiThread {
+                    timeText.text = formatWholeTime(time)
+                    hourOut.text = formatTime(hour)
+                    minOut.text = formatTime(min)
+                    secOut.text = formatTime(sec)
                 }
-                //sec == 1 -> timerTask?.cancel()
-                //else -> timerTask?.cancel()
+                if (sec == 0 && min == 0 && hour == 0) timer?.cancel()
             }
 
-            runOnUiThread {
-                timeText.text = formatTime(time)
-                hourOut.text = String.format("%02d", hour)
-                minOut.text = String.format("%02d", min)
-                secOut.text = String.format("%02d", sec)
-            }
-            if (sec == 0 && min == 0 && hour == 0) timerTask?.cancel()
-        }
+        }, 1000, 1000)
     }
 
     private fun pause() {
         fab_start.setImageResource(R.drawable.ic_play)
-        timerTask?.cancel()
+        timer?.cancel()
     }
 
     private fun reset() {
-        timerTask?.cancel()
+        timer?.cancel()
 
         time = 0
         isRunning = false
@@ -115,17 +102,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun lapTime() {
-        val lapTime = time
+        //val lapTime = time
         val textView = TextView(this).apply {
             textSize = 20f
         }
-        textView.text = formatTime(time)
+        textView.text = formatWholeTime(time)
 
         lap_Layout.addView(textView, 0)
         index++
     }
 
-    private fun formatTime(time: Int): String {
+    private fun formatWholeTime(time: Int): String {
         val hour = String.format("%02d", time / 3600)
         val time: Int = time % 3600
         val min = String.format("%02d", time / 60)
@@ -133,4 +120,13 @@ class MainActivity : AppCompatActivity() {
         return "$hour:$min:$sec"
     }
 
+    private fun formatTime(time: Int): String {
+        val time = String.format("%02d", time)
+        return "$time"
+    }
+
+    private fun strToInt(timeIn: String): Int {
+        return if (timeIn == "") 0
+        else timeIn.toInt()
+    }
 }
